e9707df97846d8b7365edab4fc1946600b7b9626
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-09-03 20:28:27 -0400

    nothing important

--- types.h
+++ types.h
@@ -13,6 +13,19 @@ typedef signed int cpu_t;
 
 typedef unsigned int memaddr;
 
+typedef struct pcb_t{
+    /*process queue fields*/
+    struct pcb_t* pnext,/*pointer to next entry*/ /*process tree fields*/ *pprnt, /*pointer to parent*/ *pchild, /*pointer to 1st child*/ *psib; /*pointer to sibling*/
+    statetps; /*processor state*/
+    int* psemAdd; /*pointer to sema4 on*/
+    /*which process blocked*/
+    /*plus other entries to be added later*/
+}pcb_t;
+
+typedef struct pcb_PTR{
+    pcb_t* pcb;
+}
+
 
 typedef struct {
 	unsigned int d_status;
