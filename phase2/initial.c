8e51c1d348a8a97e5d821df22b35a7294627019a
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2018-10-20 15:35:36 -0400

    initial.c and shceduler.c 90% done

--- phase2/initial.c
+++ phase2/initial.c
@@ -1,8 +1,79 @@
-//
-//  initial.c
-//  
-//
-//  Created by Matthew McAvey on 9/24/18.
-//
 
-#include <stdio.h>
+
+/* Global variables:
+        ProcessCount (int)
+        softBlockCount (int)
+        currentProcess (pcb_t*)
+        readyQue (pcb_t*)
+ */
+int processCount, softBlockCount;
+pcb_t* currentProcess, readyQue;
+
+
+/*where the booting happens */
+main (){
+    /* populate the four new areas in low memory
+        set the stack pointer (last page of physical)
+        set PC
+        set the status: VM: off
+                    Interupts masked
+                    Supervisor mode on
+     */
+    unsigned int topOfPM;
+    state_t* stateSetter;
+    devregarea_t* pmInfo = (devregarea_t*) RAMBASEADDR;
+    
+    topOfPM = (pmInfo->rambase) + (pmInfo->ramsize);
+    
+    /* syscallHandler */
+    stateSetter = (state_t*) 0x200003D4;
+    stateSetter->s_status = ALLOFF;
+    stateSetter->s_sp = topOfPM;
+    stateSetter->s_pc = stateSetter->s_t9 = (memaddr) syscallHandler;
+    
+    /* pgmTrap */
+    stateSetter = (state_t*) 0x200002BC;
+    stateSetter->s_status = ALLOFF;
+    stateSetter->s_sp = topOfPM;
+    stateSetter->s_pc = stateSetter->s_t9 = (memaddr) pgmTrap;
+    
+    /* tlbManager */
+    stateSetter = (state_t*) 0x200001A4;
+    stateSetter->s_status = ALLOFF;
+    stateSetter->s_sp = topOfPM;
+    stateSetter->s_pc = stateSetter->s_t9 = (memaddr) tlbManager;
+    
+    /* interuptHandler */
+    stateSetter = (state_t*) 0x2000008C;
+    stateSetter->s_status = ALLOFF;
+    stateSetter->s_sp = topOfPM;
+    stateSetter->s_pc = stateSetter->s_t9 = (memaddr) interuptHandler;
+    
+    /* initialize phase1 */
+    initPcbs();
+    initASL();
+    
+    /* initialize phase 2 global variables */
+    processCount = 0;
+    softBlockCount = 0;
+    currentProcess = NULL;
+    readyQue = mkEmptyProcQ();
+     /*
+     p = allocatePCB ()
+        initialize the p_s
+            stack pointer to the penultimate page of physical memory
+            PC (p2test)
+            status
+                VM off
+                Interupts enabled
+                supervisor mode on
+     */
+    currentProcess = allocPCB();
+    currentProcess->p_state.s_sp = (topOfPM - PAGESIZE);
+    currentProcess->p_state.s_pc = currentProcess->p_state.s_t9 = (memaddr) test;
+    currentProcess->p_state.s_status = ;
+    
+    processCount++;
+    insertProcQ(&readyQue, currentProcess);
+    scheduler();
+}
--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,8 +1,31 @@
-//
-//  scheduler.c
-//  
-//
-//  Created by Matthew McAvey on 9/24/18.
-//
 
-#include <stdio.h>
+
+
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../e/initial.e"
+#include "../e/pcb.e"
+#include "/usr/local/include/umps2/umps/libumps.e"
+
+extern int processCount, softBlockCount;
+extern pcb_t* currentProcess, readyQue;
+
+void scheduler () {
+    if (emptyProcQ(readyQue)){
+        if (processCount == 0){
+            HALT();
+        }
+        if (processCount > 0){
+            if (softBlockCount == 0){
+                PANIC ();
+            }
+            if (softBlockCount > 0){
+                WAIT();
+            }
+    } else {
+        currentProcess = removeProcQ(readyQue);
+        /* lead a timer with the value of a quantum */
+        
+        LDST(&(currentProcess->p_state));
+    }
+}
